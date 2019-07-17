a13f0275d74d75d8e723c646ffed0b591c38ab79
Jonathan Walker <jonathan.walker@sas.com>
2019-07-17 15:15:04 -0400

    cleanup

--- src/sasctl/services.py
+++ src/sasctl/services.py
@@ -4,15 +4,6 @@
 # Copyright © 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-__all__ = ['files',
-           'folders',
-           'microanalytic_score',
-           'model_management',
-           'model_publish',
-           'model_repository',
-           'projects'
-           ]
-
 
 def _instantiate(name):
     # Instantiate and return the specified class without cluttering up the
