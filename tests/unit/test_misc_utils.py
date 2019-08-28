3e6906129dc2338715355f83776e2b36c44a3bc5
jlwalke2 <jonathan.walker@sas.com>
2019-08-28 12:11:27 -0400

    list installed packages

---
+++ tests/unit/test_misc_utils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright © 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import re
+
+
+def test_list_packages():
+    from sasctl.utils.misc import installed_packages
+    packages = list(installed_packages())
+
+    # We know that these packages should always be present
+    assert any(re.match('requests==.*', p) for p in packages)
+    assert any(re.match('sasctl==.*', p) for p in packages)
+    assert any(re.match('pytest==.*', p) for p in packages)
