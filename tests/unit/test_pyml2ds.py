265c48b47d24febd24113fe208aeb5df5140fb11
Alexey Elnatanov <invalpoint@gmail.com>
2019-10-02 14:56:44 +0300

    Fixed tests

--- tests/unit/test_pyml2ds.py
+++ tests/unit/test_pyml2ds.py
@@ -17,7 +17,7 @@ DATA_PATH = os.path.join(dirname(dirname(__file__)), 'pyml2ds_data')
 
 def test_xgb2ds(tmpdir):
     IN_PKL = os.path.join(DATA_PATH, 'xgb.pkl')
-    OUT_SAS = os.path.join(tmpdir, 'xgb.sas')
+    OUT_SAS = os.path.join(str(tmpdir), 'xgb.sas')
     EXPECTED_SAS = os.path.join(DATA_PATH, 'xgb.sas')
 
     pyml2ds(IN_PKL, OUT_SAS)
@@ -28,7 +28,7 @@ def test_xgb2ds(tmpdir):
 
 def test_lgb2ds(tmpdir):
     IN_PKL = os.path.join(DATA_PATH, 'lgb.pkl')
-    OUT_SAS = os.path.join(tmpdir, 'lgb.sas')
+    OUT_SAS = os.path.join(str(tmpdir), 'lgb.sas')
     EXPECTED_SAS = os.path.join(DATA_PATH, 'lgb.sas')
 
     pyml2ds(IN_PKL, OUT_SAS)
@@ -39,7 +39,7 @@ def test_lgb2ds(tmpdir):
 
 def test_gbm2ds(tmpdir):
     IN_PKL = os.path.join(DATA_PATH, 'gbm.pmml')
-    OUT_SAS = os.path.join(tmpdir, 'gbm.sas')
+    OUT_SAS = os.path.join(str(tmpdir), 'gbm.sas')
     EXPECTED_SAS = os.path.join(DATA_PATH, 'gbm.sas')
 
     pyml2ds(IN_PKL, OUT_SAS)
