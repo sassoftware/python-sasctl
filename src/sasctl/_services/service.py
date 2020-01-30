3be8c6e7168942958b0b5601f91756952487f383
jlwalke2 <jonathan.walker@sas.com>
2020-01-30 14:55:05 -0500

    check swat build

--- src/sasctl/_services/service.py
+++ src/sasctl/_services/service.py
@@ -342,7 +342,8 @@ class Service(object):  # skipcq PYL-R0205
         return [classmethod(f) for f in
                 (list_items, get_item, update_item, delete_item)]
 
-    def _get_rel(self, item, rel, func=None, filter=None, *args):
+    def _get_rel(self, item, rel, func=None, filter=None,
+                 *args):  # skipcq PYL-W1113 - py27 compatibility
         """Get `item` and request a link.
 
         Parameters
