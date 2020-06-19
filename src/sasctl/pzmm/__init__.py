423d2ac213eedba25245de142a64aa1003239ba2
Scott Lindauer <60105574+smlindauer@users.noreply.github.com>
2020-06-19 11:38:39 -0400

    Update __init__.py
    Remove version, name, and dev built-in values for pzmm

--- src/sasctl/pzmm/__init__.py
+++ src/sasctl/pzmm/__init__.py
@@ -1,11 +1,6 @@
 # Copyright (c) 2020, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-
-__name__ = 'pzmm'
-__version__ = '1.0'
-__dev__ = True
-
 from .pickleModel import PickleModel
 from .uploadData import ModelImport
 from .writeJSONFiles import JSONFiles
