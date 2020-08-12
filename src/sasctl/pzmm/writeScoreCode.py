b0f56f2d7e76a63158fbe83e50d0d1fecac38d35
Scott Lindauer <Scott.Lindauer@sas.com>
2020-08-12 14:56:48 -0400

    Adjust score code for invalid column names/values.

--- src/sasctl/pzmm/writeScoreCode.py
+++ src/sasctl/pzmm/writeScoreCode.py
@@ -122,7 +122,7 @@ class ScoreCode():
         # From the input dataframe columns, create a list of input variables, then check for viability
         inputVarList = list(inputDF.columns)
         for name in inputVarList:
-            if not str(name).isidentifier():
+            if not name.isidentifier():
                 raise SyntaxError('Invalid column name in inputDF. Columns must be ' +
                                   'valid as Python variables.')
         newVarList = list(inputVarList)
