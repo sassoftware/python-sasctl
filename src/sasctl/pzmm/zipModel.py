124d9fa61fe13524840a9f46bd385380e139b284
Jon Walker <jonathan.walker@sas.com>
2020-10-27 14:37:56 -0400

    Update zipModel.py

--- src/sasctl/pzmm/zipModel.py
+++ src/sasctl/pzmm/zipModel.py
@@ -1,11 +1,9 @@
 # Copyright (c) 2020, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-# %%
 from pathlib import Path
 import zipfile
 
-# %%
 
 class ZipModel():
     
