a24a66662a66a628339a404a73ec381d9172bd03
Jon Walker <jonathan.walker@sas.com>
2020-10-27 14:42:27 -0400

    Update uploadData.py

--- src/sasctl/pzmm/uploadData.py
+++ src/sasctl/pzmm/uploadData.py
@@ -1,15 +1,12 @@
 # Copyright (c) 2020, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-
-# %%
 from pathlib import Path
 
 import requests
 import getpass
 import json
 
-# %%
 
 def getAccessToken(server, username=None, password=None):
     '''
