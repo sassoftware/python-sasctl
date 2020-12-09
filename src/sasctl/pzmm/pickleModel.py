404d8be2d03ecb555d5557a75337c5abb4644342
jlwalke2 <jonathan.walker@sas.com>
2020-12-09 13:05:24 -0500

    cleanup
    pickleTrainedModel should be static not object-bound.
    Fixed errors in docstring.

--- src/sasctl/pzmm/pickleModel.py
+++ src/sasctl/pzmm/pickleModel.py
@@ -1,35 +1,31 @@
 # Copyright (c) 2020, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-
-# %%
+import pickle
 from pathlib import Path
 
-import pickle
 
-# %%
-class PickleModel():
-    
-    def pickleTrainedModel(self, trainedModel, modelPrefix, pPath=Path.cwd()):
-        '''
-        Write trained model to a binary pickle file. 
+class PickleModel:
+    @staticmethod
+    def pickleTrainedModel(trainedModel, modelPrefix, pPath=Path.cwd()):
+        """Write trained model to a binary pickle file.
         
         Parameters
-        ---------------
+        ----------
         trainedModel
             User-defined trained model.
         modelPrefix : string
             Variable name for the model to be displayed in SAS Open Model Manager 
             (i.e. hmeqClassTree + [Score.py || .pickle]).
-        pPath : string, optional
+        pPath : pathlib.Path, optional
             File location for the output pickle file. Default is the current
             working directory.
-			
-		Yields
-		---------------
-		'*.pickle'
-			Binary pickle file containing a trained model.
-        '''
+
+        Returns
+        -------
+        None
+
+        """
         
         with open(pPath / (modelPrefix + '.pickle'), 'wb') as pFile:
             pickle.dump(trainedModel, pFile)
