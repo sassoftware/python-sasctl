fecd6f061e9f3818abac79b035f398517fe9c5aa
Jon Walker <jonathan.walker@sas.com>
2021-08-17 17:04:22 -0400

    Migrate from Travis-CI to GH Actions (#90)
    * Create codeql-analysis.yml
    
    * remove travis CI.  add GH build/test actions
    
    * fix package name match when installed locally
    
    * remove utf8 config
    
    * install kerberos client
    
    * kerberos dev libraries
    
    * cleanup
    
    * allow lint failure.  temporarily disable testing.
    
    * lint src directory
    
    * cleanup
    
    * build documentation
    
    * fix doc build
    
    * run codecov
    
    * install codecov
    
    * build on push (temporary)
    
    * install codecov
    
    * draft build phase
    
    * build package
    
    * archive output
    
    * combined publish steps
    
    * matrix runner OS
    
    * download doc artifact

--- tests/unit/test_misc_utils.py
+++ tests/unit/test_misc_utils.py
@@ -13,5 +13,5 @@ def test_list_packages():
 
     # We know that these packages should always be present
     assert any(re.match('requests==.*', p) for p in packages)
-    assert any(re.match('sasctl==.*', p) for p in packages)
+    assert any(re.match('sasctl.*', p) for p in packages)  # sasctl may be installed from disk so no '=='
     assert any(re.match('pytest==.*', p) for p in packages)
