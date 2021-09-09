484da59801633faae983daf8b14975ca720135d3
jlwalke2 <jonathan.walker@sas.com>
2021-09-09 11:36:30 -0400

    code qual improvements

--- src/sasctl/utils/astore.py
+++ src/sasctl/utils/astore.py
@@ -351,14 +351,18 @@ def _get_model_properties(result):
         algorithm = None
 
     def is_classification(r):
+        """Determine if the ASTORE model describes a classification model."""
         return classification_target(r) is not None
 
     def classification_target(r):
+        """Get the name of the classification target variable."""
         target = r.OutputVariables.Name[r.OutputVariables.Name.str.startswith('I_')]
         if target.shape[0] > 0:
             return target.iloc[0].replace('I_', '', 1)
+        return None
 
     def regression_target(r):
+        """Get the name of the regression target variable."""
         target = r.OutputVariables.Name.str.startswith('P_')
         target = r.OutputVariables.Name[target].iloc[0]
         return target.replace('P_', '', 1)
