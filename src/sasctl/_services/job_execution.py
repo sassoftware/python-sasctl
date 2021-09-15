fa1f16fff63ad318cf1d75d1ccd88fd74d275f25
jlwalke2 <jonathan.walker@sas.com>
2021-09-15 11:45:55 -0400

    get job state

--- src/sasctl/_services/job_execution.py
+++ src/sasctl/_services/job_execution.py
@@ -6,6 +6,7 @@
 
 
 from .service import Service
+from .job_definitions import JobDefinitions
 
 
 class JobExecution(Service):
@@ -17,6 +18,8 @@ class JobExecution(Service):
     """
 
     _SERVICE_ROOT = '/jobExecution'
+
+    list_jobs, get_job, update_job, delete_job = Service._crud_funcs('/jobs')
 
     @classmethod
     def create_job(cls, definition, name=None, description=None, parameters=None):
@@ -39,22 +42,46 @@ class JobExecution(Service):
 
         """
 
-        # TODO: definition id not RestObj passed
-        # TODO: get link fails
-        uri = cls.get_link(definition, 'self')['uri']
+        # Convert id/name into full definition object
+        definition_obj = JobDefinitions.get_definition(definition)
+
+        if definition_obj is None:
+            raise ValueError("Unable to find job definition '%s'." % definition)
+
+        uri = cls.get_link(definition_obj, 'self')['uri']
 
         parameters = parameters or {}
-        # TODO: expiresAfter - set to reasonable default
 
         data = {
             'name': name,
             'description': description,
             'jobDefinitionUri': uri,
-            'arguments': parameters
+            'arguments': parameters,
         }
 
         headers = {
             'Accept': 'application/vnd.sas.job.execution.job+json',
-            'Content-Type': 'application/vnd.sas.job.execution.job.request+json'
+            'Content-Type': 'application/vnd.sas.job.execution.job.request+json',
         }
         return cls.post('/jobs', headers=headers, json=data)
+
+    @classmethod
+    def get_job_state(cls, job):
+        """Check the status of an existing job.
+
+        Parameters
+        ----------
+        job : str or dict
+            The name or id of the job, or a dictionary representation of the job.
+
+        Returns
+        -------
+        str
+            {'pending', 'running', 'canceled', 'completed', failed'}
+
+        """
+
+        job_obj = cls.get_job(job)
+        uri = '/jobs/%s/state' % job_obj.id
+
+        return cls.get(uri)
