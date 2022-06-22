da592a69ab95ec559dc58ee801731823bdf52673
Scott Lindauer <Scott.Lindauer@sas.com>
2022-06-22 12:04:36 -0400

    Update sas regression example to use Session.as_swat()

--- examples/register_sas_regression_model.py
+++ examples/register_sas_regression_model.py
@@ -4,37 +4,39 @@
 # Copyright © 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-import swat
 from sasctl import Session
 from sasctl.tasks import register_model, publish_model
 
+hostname = ''
+username = ''
+password = ''
 
-with swat.CAS('hostname', 5570, 'username', 'password') as cas:
-    # Load the regression actions in CAS
-    cas.loadactionset('regression')
+# Use sasctl to connect to SAS
+sess = Session('hostname', 'username', 'password', protocol='http')
+cas = sess.as_swat()
 
-    # Upload the local CSV file to a CAS table
-    tbl = cas.upload('data/boston_house_prices.csv').casTable
+# Load the regression actions in CAS
+cas.loadactionset('regression')
 
-    # Model input features are everything except the target
-    features = tbl.columns[tbl.columns != 'medv']
+# Upload the local CSV file to a CAS table
+tbl = cas.upload('data/boston_house_prices.csv').casTable
 
-    # Fit a linear regression model in CAS and output an ASTORE
-    tbl.glm(target='medv', inputs=list(features), savestate='model_table')
+# Model input features are everything except the target
+features = tbl.columns[tbl.columns != 'medv']
 
-    astore = cas.CASTable('model_table')
+# Fit a linear regression model in CAS and output an ASTORE
+tbl.glm(target='medv', inputs=list(features), savestate='model_table')
 
-    # Use sasctl to connect to SAS
-    Session('hostname', 'username', 'password')
+astore = cas.CASTable('model_table')
 
-    # Register the model in SAS Model Manager, creating the "Boston Housing"
-    # project if it doesn't already exist
-    model = register_model(astore, 'Linear Regression', 'Boston Housing', force=True)
+# Register the model in SAS Model Manager, creating the "Boston Housing"
+# project if it doesn't already exist
+model = register_model(astore, 'Linear Regression', 'Boston Housing', force=True)
 
-    # Publish the model to a real-time scoring engine
-    module = publish_model(model, 'maslocal')
+# Publish the model to a real-time scoring engine
+module = publish_model(model, 'maslocal')
 
-    # Pass a row of data to MAS and receive the predicted result.
-    first_row = tbl.head(1)
-    result = module.score(first_row)
-    print(result)
+# Pass a row of data to MAS and receive the predicted result.
+first_row = tbl.head(1)
+result = module.score(first_row)
+print(result)
