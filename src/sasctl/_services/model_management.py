4ae8958f52a60f43d29b6899cfc50d5ee14ac1b2
Scott Lindauer <Scott.Lindauer@sas.com>
2022-07-25 09:52:03 -0400

    Better documentation for create_custom_kpi function

--- src/sasctl/_services/model_management.py
+++ src/sasctl/_services/model_management.py
@@ -399,25 +399,35 @@ class ModelManagement(Service):
     def create_custom_kpi(
         cls, model, project, timeLabel, kpiName, kpiValue, timeSK=None
     ):
-        """_summary_
+        """Post a user supplied custom KPI to a SAS Model Manager project's MM_STD_KPI
+        table. A custom KPI consists of the time label, KPI name, KPI value, and optionally
+        the timeSK. Additionally, the model and project associated with the custom KPI are
+        required.
+
+        Multiple custom KPIs can be uploaded at once by passing lists in for the four
+        arguments mentioned above.
 
         Parameters
         ----------
-        model : _type_
-            _description_
-        project : _type_
-            _description_
-        timeLabel : _type_
-            _description_
-        kpiName : _type_
-            _description_
-        kpiValue : _type_
-            _description_
+        model : str or dict
+            The name or id of the model, or a dictionary representation of the model.
+        project : str or dict
+        The name or id of the project, or a dictionary representation of
+        the project.
+        timeLabel : str or list
+            Label associated with the dataset used within the performance definition.
+        kpiName : str or list
+            Name of the custom KPI.
+        kpiValue : int or float or list
+            Value of the custom KPI.
+        timeSK : int or list, by default None
+            Indicator for the MM_STD_KPI table to denote performance task order.
         """
         from .model_repository import ModelRepository
 
         mr = ModelRepository()
 
+        # Step through options to determine project UUID
         if cls.is_uuid(project):
             projectId = project
         elif isinstance(project, dict) and "id" in project:
@@ -426,6 +436,7 @@ class ModelManagement(Service):
             project = mr.get_project(project)
             projectId = project["id"]
 
+        # Step through options to determine model UUID
         if cls.is_uuid(model):
             modelId = model
         elif isinstance(model, dict) and "id" in model:
@@ -438,15 +449,18 @@ class ModelManagement(Service):
 
         model = mr.get_model(modelId)
 
+        # If no timeSK is provided, create a list of 0's for the API call
         if not timeSK:
             timeSK = [0] * len(timeLabel)
 
+        # Create a list of dicts mapped to each custom KPI value
         customKPI = [
             {"TimeLabel": label, "KPI": name, "Value": str(value), "TimeSK": SK}
             for label, name, value, SK in zip(timeLabel, kpiName, kpiValue, timeSK)
         ]
         headers = {"Accept": "application/vnd.sas.collection+json"}
         requestData = {"ProjectID": projectId, "ModelID": modelId, "KPIs": customKPI}
+        # Include a terminal output, since it can take up to 60 seconds to POST the API
         print("Uploading custom kpis to SAS Viya...")
         return cls.post(
             "/projects/{}/kpis".format(projectId),
