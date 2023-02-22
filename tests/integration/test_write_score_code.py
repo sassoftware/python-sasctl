993c7a56df5773ecd3b6ed98acffe34409b0c006
Scott Lindauer <Scott.Lindauer@sas.com>
2023-02-22 13:06:49 -0500

    write_score_code.py integration tests and cassettes

---
+++ tests/integration/test_write_score_code.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright © 2023, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from pathlib import Path
+import pytest
+import tempfile
+
+import pandas as pd
+
+from sasctl import current_session
+from sasctl.pzmm.write_score_code import ScoreCode as sc
+from sasctl._services.model_repository import ModelRepository as mr
+
+pytestmark = pytest.mark.usefixtures("session")
+
+
+@pytest.fixture()
+def fake_predict():
+    pass
+
+
+def example_model(data):
+    input_vars = [{"name": x, "type": "decimal", "role": "input"}
+                  for x in data.columns.to_list()]
+    output_vars = [{"name": "Classification", "type": "decimal", "role": "output"},
+                   {"name": "Prediction", "type": "decimal", "role": "output"}]
+    project = mr.create_project(
+        project="TestProject",
+        repository=mr.default_repository().get("id"),
+        variables=input_vars + output_vars
+    )
+    model = mr.create_model(
+        model="TestModel",
+        project=project,
+        score_code_type="Python",
+        input_variables=input_vars,
+        output_variables=output_vars
+    )
+    return model
+
+
+def test_write_score_code(hmeq_dataset):
+    """
+    Test Cases:
+    - Python score code is uploaded successfully
+    - DS2 wrapper is created and scoreCodeType property updates
+    - MAS/CAS code uploaded successfully and scoreCodeType property updated
+    - files exist in output_dict
+    """
+    if current_session().version_info() == 4:
+        pytest.skip(
+            "The write_score_code function does not make any API calls for SAS Viya 4."
+        )
+    input_data = hmeq_dataset.drop(columns=["BAD"])
+    model = example_model(input_data)
+    output_dict = sc.write_score_code(
+        "TestModel",
+        input_data,
+        fake_predict,
+        ["Classification", "Prediction"],
+        model=model,
+        binary_string=b"BinaryStringModel"
+    )
+
+    assert "TestModel_score.py" in output_dict
+    assert "dmcas_epscorecode.sas" in output_dict
+    assert "dmcas_packagescorecode.sas" in output_dict
+
