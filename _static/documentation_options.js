8fe6a4524d0ab54a6931502e13ee927f19efc3af
smlindauer <smlindauer@users.noreply.github.com>
2023-04-05 12:55:58 +0000

    deploy: af9a468f18fdbffbcf1e8fecf06bf9d6515bfa44

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,14 +1,14 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.8.2',
+    VERSION: '1.9.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
-    HAS_SOURCE: true,
+    HAS_SOURCE: false,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false,
+    NAVIGATION_WITH_KEYS: true,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
