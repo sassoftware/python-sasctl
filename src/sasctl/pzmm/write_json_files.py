5aafda186c30cb3ccf6b4c811150471663a0a505
Scott Lindauer <Scott.Lindauer@sas.com>
2023-04-26 12:13:20 -0400

    Black reformatting

--- src/sasctl/pzmm/write_json_files.py
+++ src/sasctl/pzmm/write_json_files.py
@@ -125,6 +125,7 @@ class JSONFiles:
                     if isinstance(obj, np.ndarray):
                         return obj.tolist()
                     return json.JSONEncoder.default(self, obj)
+
         except ImportError:
             np = None
 
