44dc0fb19015bec687c476fb1a60230466b29ce7
samyarpotlapalli <posa@unc.edu>
2024-06-04 16:42:14 -0400

    "Testing."

--- score_execution.py
+++ score_execution.py
@@ -11,7 +11,7 @@ DEFAULT_SERVER = "cas-shared-default"
 DEFAULT_CASUSER = "Public"
 DEFAULT_TABLE = ""
 
-def score_definition(score_def_name: str,  model_id: str, table_name: str = "", description:str = "", server_name: str = "cas-shared-default", library_name: str = "Public", model_version: str = "latest"):
+def score_execution(score_def_name: str,  model_id: str, table_name: str, description:str = "", server_name: str = DEFAULT_SERVER, library_name: str = DEFAULT_CASUSER, model_version: str = "latest"):
     """Creates the score definition service.
     
     Parameters
@@ -21,7 +21,7 @@ def score_definition(score_def_name: str,  model_id: str, table_name: str = "", 
     model_id: str
         A user-inputted model that exists in a project.
     table_name: str
-        A user-inputted table name in CAS Management. Defaults to an empty string.
+        A user-inputted table name in CAS Management.
     description: str
         Description of score definition. Defaults to an empty string.
     server_name: str, optional
@@ -36,10 +36,5 @@ def score_definition(score_def_name: str,  model_id: str, table_name: str = "", 
     RestObj
 
     """
-
-    try:
-        model = sess.get(f"modelRepository/models/{model_id}")
-        model_project_id = model.json()["projectId"]
-        
-    except:
-        print("This model may not exist in a project.") #tested -> should also catch if model just doesn't exist
+    
+    all_score_executions = sess.get("scoreExecution/executions")
