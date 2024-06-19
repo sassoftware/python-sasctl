1461ded78ad32d54c45f2c552b601f5faa1e5574
samyarpotlapalli <posa@unc.edu>
2024-06-19 16:55:11 -0400

    Adding comments and adjusting for various API call and responses

--- score_definition.py
+++ score_definition.py
@@ -19,17 +19,17 @@ def score_definition(score_def_name: str,  model_id: str, table_name: str, descr
     score_def_name: str
         Name of score definition.
     model_id: str
-        A user-inputted model id where the model exists in a project.
+        A user-inputted model if where the model exists in a project.
     table_name: str
-        A user-inputted table name in CAS Management.
+        A user-inputted table name in CAS Management.  
     description: str, optional
         Description of score definition. Defaults to an empty string.
     server_name: str, optional
-        The server within CAS that the table is in. Defaults to "cas-shared-default"
+        The server within CAS that the table is in. Defaults to "cas-shared-default".
     library_name: str, optional
-        The library within the CAS server the table exists in. Defaults to "Public"
+        The library within the CAS server the table exists in. Defaults to "Public".
     model_version: str, optional
-        Defaults to "latest". 
+        The user-chosen version of the model with the specified model_id. Defaults to "latest". 
     
     Returns
     -------
@@ -44,25 +44,31 @@ def score_definition(score_def_name: str,  model_id: str, table_name: str, descr
         model_name = model.json()["name"]
         
     except:
-        print("This model may not exist in a project.")
-
-    #mapping
-    inputMapping = []
-    for input_item in model.json()["inputVariables"]:
-        var = {"mappingValue":input_item["name"],"mappingType":"datasource","variableName":input_item["name"]}
-        inputMapping.append(var)
+        print("This model may not exist in a project or the model may not exist at all.")
+    # Checking if the model exists and if it's in a project
     
+    try:
+        inputMapping = []
+        for input_item in model.json()["inputVariables"]:
+            var = {"mappingValue":input_item["name"],"mappingType":"datasource","variableName":input_item["name"]}
+            inputMapping.append(var)
+    except:
+        print("This model does not have the optional 'inputVariables' parameter.")
+    
+    # Optional mapping - Maps the variables in the data to the variables of the score object. It's not necessary to create a score definition.
+  
     table = sess.get(f"casManagement/servers/{server_name}/caslibs/{library_name}/tables/{table_name}")
     try:
-        print(table.json()) #because table_name is an empty string/none, it's ignore the last part of the uri and just printing all the tables, that's why error code 400 is not being recognized
         table_status_code = table.json()['httpStatusCode']
         if table_status_code == 404 or table_status_code == 400:
             table_file = input("The inputted table does not exist. Enter the file path for the data in your table (CSV, XLS, XLSX, SAS7BDT or SASHDAT file).")
-            cas.upload_file(str(table_file), table_name)
+            cas.upload_file(str(table_file), table_name) #do I need to add a check if the file doesn't exist or does upload_file take care of that?
             table = sess.get(f"casManagement/servers/{server_name}/caslibs/{library_name}/tables/{table_name}")
+            # Checks if the inputted table exists, and if not, uploads a file to create a new table
     except:
-        print("Table GET API call successful due to no 'httpStatusCode' key.")
-    
+        pass 
+        #The API call to get the table was successful, and therefore will not have an 'httpStatusCode' value. This 'pass' statement will allow the successful get API calls to proceed.
+
     save_score_def = {"name": score_def_name,
                 "description": description,
                 "objectDescriptor":{"uri":f"/modelManagement/models/{model_id}",
@@ -78,8 +84,8 @@ def score_definition(score_def_name: str,  model_id: str, table_name: str, descr
                               "projectVersionUri": f"/modelRepository/projects/{model_project_id}/projectVersions/{model_project_version_id}",
                               "publishDestination":"",
                               "versionedModel": f"{model_name}({model_version})"},
-                "mappings": inputMapping} # Getting the input variables from the model that eventually have to be mapped to the input table created/retrieved from CAS Management
-                                        #Array of mappings between Score Object variables and Input Data columns.
+                "mappings": inputMapping}
+    # Consolidating all of the model and table information to create the score definition information
     
     headers_score_def = {
     'Content-Type': "application/json"
@@ -88,3 +94,4 @@ def score_definition(score_def_name: str,  model_id: str, table_name: str, descr
     new_score_def = sess.post("scoreDefinitions/definitions", data=json.dumps(save_score_def), headers=headers_score_def)
 
     return new_score_def
+    # The response information of the score definition can be seen as a JSON as well as the RestOBJ
--- score_execution.py
+++ score_execution.py
@@ -7,34 +7,49 @@ from typing import Union
 from sasctl import Session
 from sasctl._services.cas_management import CASManagement as cas
 
-DEFAULT_SERVER = "cas-shared-default"
-DEFAULT_CASUSER = "Public"
-DEFAULT_TABLE = ""
-
-def score_execution(score_def_name: str,  model_id: str, table_name: str, description:str = "", server_name: str = DEFAULT_SERVER, library_name: str = DEFAULT_CASUSER, model_version: str = "latest"):
+def score_execution(score_definition, description: str = "", output_server_name: str = "cas-shared-default", output_library_name: str = "Public", output_table_name: str = ""):
     """Creates the score definition service.
     
     Parameters
     --------
-    score_def_name: str
-        Name of score definition
-    model_id: str
-        A user-inputted model that exists in a project.
-    table_name: str
-        A user-inputted table name in CAS Management.
+    score_definition
+        Score Definition RestObj.
     description: str
         Description of score definition. Defaults to an empty string.
-    server_name: str, optional
-        The server within CAS that the table is in. Defaults to "cas-shared-default"
-    library_name: str, optional
-        The library within the CAS server the table exists in. Defaults to "Public"
-    model_version: str, optional
-        Defaults to "latest". 
+    output_server_name: str, optional
+        The server within CAS that the output table is in. Defaults to "cas-shared-default".
+    output_library_name: str, optional
+        The library within the CAS server the output table exists in. Defaults to "Public".
+    output_table_name: str, optional
+        The name of the output table. Defaults to an empty string.
+     
     
     Returns
     -------
     RestObj
 
     """
+    try:
+        score_definition_id = score_definition.json()["id"]
+        score_definition_name = score_definition.json()["name"]
+        score_definition_model_id = score_definition.json["modelId"]
+        score_definition_model_name = score_definition.json["modelName"]
+    except:
+        print("The input score definition object may not exist. Please check it was passed in correctly")
+
+    if output_table_name == "":
+        output_table_name = f"{score_definition_model_name}_{score_definition_id}"
+
+    try:
+        filtered_score_executions = sess.get(f"scoreExecution/executions?filter=eq(scoreExecutionRequest.scoreDefinitionId,%27{score_definition_id}%27)")
+        deleted_score_execution = sess.delete(f"scoreExecution/executions/{filtered_score_executions["items"][0]["id"]}")
+    #can there be more than one score execution for a score definition running (execCount >= 1)
+    except:
+        pass
+
     
-    all_score_executions = sess.get("scoreExecution/executions")
+            
+
+
+
+
