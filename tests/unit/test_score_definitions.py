e433643d62433ca04ac7af2f9cf8039312f1230d
samyarpotlapalli <posa@unc.edu>
2024-07-17 14:54:19 -0400

    First version of all test cases for score definition

--- tests/unit/test_score_definitions.py
+++ tests/unit/test_score_definitions.py
@@ -23,132 +23,111 @@ from sasctl import current_session
 from sasctl.core import RestObj, VersionInfo, request
 from sasctl._services.score_definitions import ScoreDefinitions as sd
 
+
 def test_create_score_definition():
     """
     Test Cases:
-    - Valid model id?
-        - yes
-        - no
-    - Valid input mapping?
-        - yes 
-        - no
-    -Valid table name?
-        -yes
-        -no
-    -Valid file?
-        -yes
-        -no
+    - Valid model id with input mapping and valid table name
+    - Invalid model id
+    - Valid table name without input mapping
+    - Invalid table name with invalid file
+    - Invalid table name with valid file and without input mapping
+
     """
-    
+    with mock.patch("sasctl.core.Session._get_authorization_token"):
+        current_session("example.com", "username", "password")
+
     with mock.patch(
         "sasctl._services.model_repository.ModelRepository.get_model"
     ) as get_model:
-         with mock.patch(
+        with mock.patch(
             "sasctl._services.cas_management.CASManagement.get_table"
         ) as get_table:
             with mock.patch(
                 "sasctl._services.cas_management.CASManagement.upload_file"
             ) as upload_file:
-                get_model.return_value.status_code = 404
-                with pytest.raises(HTTPError):
-                    sd.create_score_definition(
-                        score_def_name = "test_create_sd",
-                        model_id = "12345",
-                        table_name = "test_table"
-                    )
-                # get_model.return_value.status_code = 200
-                # get_model.return_value.json.return_value = {
-                #     "id": "12345",
-                #     "projectId": "54321",
-                #     "projectVersionId": "67890",
-                #     "name": "test_model"
-                #     "inputVariables": {"A", "B", "C"} #look at syntax combine square brackets and curly bracket
-                # }
-                #     sd.create_score_definition(
-                #         score_def_name = "test_create_sd",
-                #         model_id = "12345",
-                #         table_name = "test_table",
-                #         mappings = {
-                #             {"mappingVariable": "A", "datasource": "datasource", "variableName": "A"},
-                #             {"mappingVariable": "B", "datasource": "datasource", "variableName": "B"},
-                #             {"mappingVariable": "C", "datasource": "datasource", "variableName": "C"}
-                #         }
-                #     )
-                    
-                get_model.return_value.status_code = 200
-                get_model.return_value.json.return_value = {
-                    "id": "12345",
-                    "projectId": "54321",
-                    "projectVersionId": "67890",
-                    "name": "test_model",
-                }
-                get_table.return_value.status_code = 404
-                with pytest.raises(HTTPError):
-                    sd.create_score_definition(
-                            score_def_name = "test_create_sd",
-                            model_id = "12345",
-                            table_name = "test_table"
-                    )
-                get_table.return_value.status_code = 404
-                upload_file.return_value = None
-                get_table.return_value.status_code = 404
-                with pytest.raises(HTTPError):
-                    sd.create_score_definition(
-                            score_def_name = "test_create_sd",
-                            model_id = "12345",
-                            table_name = "test_table",
-                            table_file = "test_path"
+                with mock.patch(
+                    "sasctl._services.score_definitions.ScoreDefinitions.post"
+                ) as post:
+                    get_model.return_value.status_code = 404
+                    with pytest.raises(HTTPError):
+                        sd.create_score_definition(
+                            score_def_name="test_create_sd",
+                            model_id="12345",
+                            table_name="test_table",
                         )
-                
-                get_table.return_value.status_code = 404
-                upload_file.return_value = RestObj
-                get_table.return_value.status_code = 200
-                response = sd.create_score_definition(
-                        score_def_name = "test_create_sd",
-                        model_id = "12345",
-                        table_name = "test_table",
-                        table_file = "test_path"
-                )
-                assert response
-                
-                
-                
-                
-                # get_table.return_value.status_code = 200
-                # get_table.return_value.json.return_value = {
-                #     "tableName": "test_table"
-                # }
-                # sd.create_score_definition(
-                #     score_def_name = "test_create_sd",
-                #     model_id = "12345",
-                #     table_name = "test_table"
-                # )
 
-                
-                
-                
-                
-         
-                # # Test for valid model id, no input mapping, get_table succeeded, and post succeeds
-               
-                # get_table.return_value = RestObj()
-                # raise_for_status.return_value = None
-                
-                
-                
-                # assert response
-                
-                # Test for invalid model id
-                #get_model.return_value = RestObj()
-                #with pytest.raises(
-                    #HTTPError
-                #): #test tget_Table since we don't know what the return value is restobj
-                    #sd.create_score_definition(
-                        #score_def_name = "test_create_sd",
-                        #model_id = "12345",
-                        #table_name = "test_table",
-                    #)
-                #Test for valid and invalid for each
-                #Start with testing invalid and realize you can work from the last call to the first because the last call working means the first call must have worked
-               
-    
+                    get_model.return_value.status_code = 200
+                    get_model.return_value.json.return_value = {
+                        "id": "12345",
+                        "projectId": "54321",
+                        "projectVersionId": "67890",
+                        "name": "test_model",
+                    }
+                    get_table.return_value.status_code = 404
+                    with pytest.raises(HTTPError):
+                        sd.create_score_definition(
+                            score_def_name="test_create_sd",
+                            model_id="12345",
+                            table_name="test_table",
+                        )
+                    get_table.return_value.status_code = 404
+                    upload_file.return_value = None
+                    get_table.return_value.status_code = 404
+                    with pytest.raises(HTTPError):
+                        sd.create_score_definition(
+                            score_def_name="test_create_sd",
+                            model_id="12345",
+                            table_name="test_table",
+                            table_file="test_path",
+                        )
+
+                    get_table.return_value.status_code = 404
+                    upload_file.return_value = RestObj
+                    get_table.return_value.status_code = 200
+                    get_table.return_value.json.return_value = {
+                        "tableName": "test_table"
+                    }
+                    response = sd.create_score_definition(
+                        score_def_name="test_create_sd",
+                        model_id="12345",
+                        table_name="test_table",
+                        table_file="test_path",
+                    )
+                    assert response
+
+                    get_table.return_value.status_code = 200
+                    get_table.return_value.json.return_value = {
+                        "tableName": "test_table"
+                    }
+                    response = sd.create_score_definition(
+                        score_def_name="test_create_sd",
+                        model_id="12345",
+                        table_name="test_table",
+                        table_file="test_path",
+                    )
+                    assert response
+
+                    get_model.return_value.status_code = 200
+                    get_model.return_value.json.return_value = {
+                        "id": "12345",
+                        "projectId": "54321",
+                        "projectVersionId": "67890",
+                        "name": "test_model",
+                        "inputVariables": [
+                            {"name": "first"},
+                            {"name": "second"},
+                            {"name": "third"},
+                        ],
+                    }
+                    get_table.return_value.status_code = 200
+                    get_table.return_value.json.return_value = {
+                        "tableName": "test_table"
+                    }
+                    response = sd.create_score_definition(
+                        score_def_name="test_create_sd",
+                        model_id="12345",
+                        table_name="test_table",
+                    )
+                    assert response
+                    assert post.call_count == 3
