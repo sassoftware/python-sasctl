c70553983712541a8bb8ad9a1adbe9f8f90fdd42
samyarpotlapalli <posa@unc.edu>
2024-07-18 15:58:08 -0400

    Updating files for testing

--- tests/unit/test_score_execution.py
+++ tests/unit/test_score_execution.py
@@ -22,6 +22,7 @@ import sasctl.pzmm as pzmm
 from sasctl import current_session
 from sasctl.core import RestObj, VersionInfo
 from sasctl._services.score_definitions import ScoreDefinitions as sd
+from sasctl._services.score_execution import ScoreExecution as se
 
 
 def test_create_score_execution():
@@ -32,9 +33,12 @@ def test_create_score_execution():
         -no
     - Valid execution id?
 
-    -Valid count key?
+    -Valid count key? -> treated like input mapping -> no because i think it's required
+    - output table -> treat like input mapping but within the create_score_execution step (do I for library and server in score definition thought?)
 
     """
+    with mock.patch("sasctl.core.Session._get_authorization_token"):
+        current_session("example.com", "username", "password")    
 
     with mock.patch(
         "sasctl._services.score_definitions.ScoreDefinitions.get_definition"
@@ -45,5 +49,21 @@ def test_create_score_execution():
             with mock.patch(
                 "sasctl._services.score_execution.ScoreExecution.delete_execution"
             ) as delete_execution:
-                pytest.skip()
+                with mock.patch(
+                   "sasctl._services.score_execution.ScoreExecution.post" 
+                ) as post:
+                    get_definition.return_value.status_code = 404
+                    with pytest.raises(HTTPError):
+                        se.create_score_execution(
+                            score_definition_id="12345",
+                        )
+                    get_definition.return_value.status_code = 200
+                    get_execution.return_value.status_code = 400 #we might need a separate try except here to show that 404 statement is weird and should exit the program
+                    with pytest.raises(HTTPError):
+                        se.create_score_execution(
+                            score_definition_id="12345",
+                        )
+                    #stdout or pytest.raises but without ending program?
+                
+                #pytest.skip()
                 # raise HTTP error?
