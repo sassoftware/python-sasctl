f30d1e5252fe66043386dc6482ec2be50012895d
Jonathan Walker <Jonathan.Walker@sas.com>
2024-08-29 15:13:38 -0400

    test: cleanup

--- tests/unit/test_pageiterator_no_paging.py
+++ tests/unit/test_pageiterator_no_paging.py
@@ -1,4 +1,3 @@
-import unittest.mock
 from unittest import mock
 
 from sasctl.core import PageIterator, RestObj
@@ -10,19 +9,7 @@ def test_no_paging_required():
     items = [{"name": "a"}, {"name": "b"}, {"name": "c"}]
     obj = RestObj(items=items, count=len(items))
 
-    import sasctl
-    assert not isinstance(sasctl.core.request, unittest.mock.Mock)
-
     with mock.patch("sasctl.core.Session.request") as req:
-    # with mock.patch("sasctl.core.request") as req:
-        req.reset_mock(side_effect=True)
-        try:
-            req.assert_not_called()
-        except AssertionError:
-            raise AssertionError(
-                f"Previous calls: method_calls={req.mock_calls}  call_args={req.call_args_list}"
-            )
-
         pager = PageIterator(obj)
 
         # Returned page of items should preserve item order
