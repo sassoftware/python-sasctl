1c3df3d3b2ccf3933093d19079106261204c1f3d
Jonathan Walker <Jonathan.Walker@sas.com>
2024-08-29 10:22:17 -0400

    test: remove incremental marker

--- tests/unit/test_pageiterator.py
+++ tests/unit/test_pageiterator.py
@@ -56,44 +56,43 @@ def paging(request):
     assert req.call_count >= math.ceil(call_count)
 
 
-@pytest.mark.incremental
-class TestPageIterator:
-    def test_no_paging_required(self):
-        """If "next" link not present, current items should be included."""
+def test_no_paging_required(self):
+    """If "next" link not present, current items should be included."""
 
-        items = [{"name": "a"}, {"name": "b"}, {"name": "c"}]
-        obj = RestObj(items=items, count=len(items))
+    items = [{"name": "a"}, {"name": "b"}, {"name": "c"}]
+    obj = RestObj(items=items, count=len(items))
 
-        with mock.patch("sasctl.core.request") as req:
-            pager = PageIterator(obj)
-
-            # Returned page of items should preserve item order
-            items = next(pager)
-            for idx, item in enumerate(items):
-                assert item.name == RestObj(items[idx]).name
-
-        # No req should have been made to retrieve additional data.
-        try:
-            req.assert_not_called()
-        except AssertionError as e:
-            raise AssertionError(
-                f"method_calls={req.mock_calls}  call_args={req.call_args_list}"
-            )
-
-    def test_paging_required(self, paging):
-        """Requests should be made to retrieve additional pages."""
-        obj, items, _ = paging
-
+    with mock.patch("sasctl.core.request") as req:
         pager = PageIterator(obj)
-        init_count = pager._start
 
-        for i, page in enumerate(pager):
-            for j, item in enumerate(page):
-                if i == 0:
-                    item_idx = j
-                else:
-                    # Account for initial page size not necessarily being same size
-                    # as additional pages
-                    item_idx = init_count + (i - 1) * pager._limit + j
-                target = RestObj(items[item_idx])
-                assert item.name == target.name
+        # Returned page of items should preserve item order
+        items = next(pager)
+        for idx, item in enumerate(items):
+            assert item.name == RestObj(items[idx]).name
+
+    # No req should have been made to retrieve additional data.
+    try:
+        req.assert_not_called()
+    except AssertionError as e:
+        raise AssertionError(
+            f"method_calls={req.mock_calls}  call_args={req.call_args_list}"
+        )
+
+
+def test_paging_required(self, paging):
+    """Requests should be made to retrieve additional pages."""
+    obj, items, _ = paging
+
+    pager = PageIterator(obj)
+    init_count = pager._start
+
+    for i, page in enumerate(pager):
+        for j, item in enumerate(page):
+            if i == 0:
+                item_idx = j
+            else:
+                # Account for initial page size not necessarily being same size
+                # as additional pages
+                item_idx = init_count + (i - 1) * pager._limit + j
+            target = RestObj(items[item_idx])
+            assert item.name == target.name
