2dcf5b2263546e9fea773c0acdd9abe9009ced57
Jonathan Walker <Jonathan.Walker@sas.com>
2024-08-29 13:30:29 -0400

    test: debug mock calls

--- tests/unit/test_pageiterator_no_paging.py
+++ tests/unit/test_pageiterator_no_paging.py
@@ -11,6 +11,13 @@ def test_no_paging_required():
 
     with mock.patch("sasctl.core.request") as req:
         req.reset_mock(side_effect=True)
+        try:
+            req.assert_not_called()
+        except AssertionError:
+            raise AssertionError(
+                f"Previous calls: method_calls={req.mock_calls}  call_args={req.call_args_list}"
+            )
+
         pager = PageIterator(obj)
 
         # Returned page of items should preserve item order
@@ -21,7 +28,7 @@ def test_no_paging_required():
     # No req should have been made to retrieve additional data.
     try:
         req.assert_not_called()
-    except AssertionError as e:
+    except AssertionError:
         raise AssertionError(
             f"method_calls={req.mock_calls}  call_args={req.call_args_list}"
         )
