818d4c9cefd7e50ea0752491de5b81c1526e0d2b
samyarpotlapalli <posa@unc.edu>
2024-10-18 15:03:58 -0400

    added cas gateway optional parameter

--- src/sasctl/_services/score_definitions.py
+++ src/sasctl/_services/score_definitions.py
@@ -4,7 +4,7 @@ import sys
 
 from pathlib import Path
 import json
-from typing import Union
+from typing import Union, Optional
 
 from ..core import current_session, delete, get, sasctl_command, RestObj
 from .cas_management import CASManagement
@@ -41,6 +41,7 @@ class ScoreDefinitions(Service):
         score_def_name: str,
         model: Union[str, dict],
         table_name: str,
+        use_cas_gateway: Optional[bool] = False,
         table_file: Union[str, Path] = None,
         description: str = "",
         server_name: str = "cas-shared-default",
@@ -57,6 +58,8 @@ class ScoreDefinitions(Service):
             The name or id of the model, or a dictionary representation of the model.
         table_name: str
             A user-inputted table name in CAS Management.
+        use_cas_gateway: bool, optional
+            Determines whether object uses CAS Gateway or not.
         table_file: str or Path, optional
             A user-provided path to an uploadable file. Defaults to None.
         description: str, optional
@@ -73,6 +76,12 @@ class ScoreDefinitions(Service):
         RestObj
 
         """
+        # Changes object descriptor type to either use or not use CAS Gateway
+        object_descriptor_type: str
+        if use_cas_gateway:
+            object_descriptor_type = "sas.models.model.python"
+        else:
+            object_descriptor_type = "sas.models.model.ds2"
 
         model = cls._model_repository.get_model(model)
 
@@ -120,12 +129,12 @@ class ScoreDefinitions(Service):
             # Checks if the inputted table exists, and if not, uploads a file to create a new table
 
         save_score_def = {
-            "name": score_def_name,
+            "name": model_name,  # used to be score_def_name
             "description": description,
             "objectDescriptor": {
                 "uri": f"/modelManagement/models/{model_id}",
                 "name": f"{model_name}({model_version})",
-                "type": "sas.models.model.python",
+                "type": f"{object_descriptor_type}",
             },
             "inputData": {
                 "type": "CASTable",
