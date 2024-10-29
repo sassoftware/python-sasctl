58ececb8d0deab54d3793f8687e6209774e7b94f
Scott-Lindauer_sasinst <Scott.Lindauer@sas.com>
2024-10-29 14:02:55 -0400

    fix: revert examples back to their state on the main branch

--- examples/data/hmeqModels/GradientBoosting/score_GradientBoosting.py
+++ examples/data/hmeqModels/GradientBoosting/score_GradientBoosting.py
@@ -16,40 +16,73 @@ def score(LOAN, MORTDUE, VALUE, YOJ, DEROG, DELINQ, CLAGE, NINQ, CLNO, DEBTINC):
         global model
     except NameError:
         with open(Path(settings.pickle_path) / "GradientBoosting.pickle", "rb") as pickle_model:
-            model = pickle.load(pickle_model)
+                model = pickle.load(pickle_model)
 
 
-    index=None
-    if not isinstance(LOAN, pd.Series):
-        index=[0]
-    input_array = pd.DataFrame(
-        {"LOAN": LOAN, "MORTDUE": MORTDUE, "VALUE": VALUE, "YOJ": YOJ, "DEROG": DEROG,
-        "DELINQ": DELINQ, "CLAGE": CLAGE, "NINQ": NINQ, "CLNO": CLNO, "DEBTINC":
-        DEBTINC}, index=index
-    )
-    input_array = impute_missing_values(input_array)
-    prediction = model.predict_proba(input_array).tolist()
+
+    try:
+        if math.isnan(LOAN):
+            LOAN = 18607.96979865772
+    except TypeError:
+        LOAN = 18607.96979865772
+    try:
+        if math.isnan(MORTDUE):
+            MORTDUE = 73760.817199559
+    except TypeError:
+        MORTDUE = 73760.817199559
+    try:
+        if math.isnan(VALUE):
+            VALUE = 101776.04874145007
+    except TypeError:
+        VALUE = 101776.04874145007
+    try:
+        if math.isnan(YOJ):
+            YOJ = 8.922268135904499
+    except TypeError:
+        YOJ = 8.922268135904499
+    try:
+        if math.isnan(DEROG):
+            DEROG = 0.2545696877380046
+    except TypeError:
+        DEROG = 0.2545696877380046
+    try:
+        if math.isnan(DELINQ):
+            DELINQ = 0.4494423791821561
+    except TypeError:
+        DELINQ = 0.4494423791821561
+    try:
+        if math.isnan(CLAGE):
+            CLAGE = 179.7662751900465
+    except TypeError:
+        CLAGE = 179.7662751900465
+    try:
+        if math.isnan(NINQ):
+            NINQ = 1.1860550458715597
+    except TypeError:
+        NINQ = 1.1860550458715597
+    try:
+        if math.isnan(CLNO):
+            CLNO = 21.29609620076682
+    except TypeError:
+        CLNO = 21.29609620076682
+    try:
+        if math.isnan(DEBTINC):
+            DEBTINC = 33.779915349235246
+    except TypeError:
+        DEBTINC = 33.779915349235246
+
+    input_array = pd.DataFrame([[LOAN, MORTDUE, VALUE, YOJ, DEROG, DELINQ, CLAGE, NINQ, CLNO, DEBTINC]],
+                              columns=["LOAN", "MORTDUE", "VALUE", "YOJ", "DEROG", "DELINQ", "CLAGE", "NINQ", "CLNO", "DEBTINC"],
+                              dtype=float)
+    prediction = model.predict_proba(input_array)
 
     # Check for numpy values and convert to a CAS readable representation
     if isinstance(prediction, np.ndarray):
-        prediction = prediction.tolist()
+        prediction = prediction.tolist()[0]
 
-    if input_array.shape[0] == 1:
-        if prediction[0][1] > 0.5:
-            EM_CLASSIFICATION = "1"
-        else:
-            EM_CLASSIFICATION = "0"
-        return EM_CLASSIFICATION, prediction[0][1]
+    if prediction[0] > prediction[1]:
+        EM_CLASSIFICATION = "1"
     else:
-        df = pd.DataFrame(prediction)
-        proba = df[1]
-        classifications = np.where(df[1] > 0.5, '1', '0')
-        return pd.DataFrame({'EM_CLASSIFICATION': classifications, 'EM_EVENTPROBABILITY': proba})
+        EM_CLASSIFICATION = "0"
 
-def impute_missing_values(data):
-    impute_values = \
-        {'DEBTINC': 33.779915349235246, 'LOAN': 18607.96979865772, 'DELINQ':
-        0.4494423791821561, 'YOJ': 8.922268135904499, 'NINQ': 1.1860550458715597,
-        'VALUE': 101776.04874145007, 'DEROG': 0.2545696877380046, 'CLNO':
-        21.29609620076682, 'CLAGE': 179.7662751900465, 'MORTDUE': 73760.817199559}
-    return data.replace('           .', np.nan).fillna(impute_values).apply(pd.to_numeric, errors='ignore')
+    return EM_CLASSIFICATION, prediction[0]
---
+++ examples/data/hmeqModels/RandomForest/score_RandomForest2.py
@@ -0,0 +1,55 @@
+import math
+import pickle
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+import settings
+
+with open(Path(settings.pickle_path) / "RandomForest2.pickle", "rb") as pickle_model:
+    model = pickle.load(pickle_model)
+
+def score(LOAN, MORTDUE, VALUE, YOJ, DEROG, DELINQ, CLAGE, NINQ, CLNO, DEBTINC):
+    "Output: EM_CLASSIFICATION, EM_EVENTPROBABILITY"
+
+    try:
+        global model
+    except NameError:
+        with open(Path(settings.pickle_path) / "RandomForest2.pickle", "rb") as pickle_model:
+            model = pickle.load(pickle_model)
+
+
+    index=None
+    if not isinstance(LOAN, pd.Series):
+        index=[0]
+    input_array = pd.DataFrame(
+        {"LOAN": LOAN, "MORTDUE": MORTDUE, "VALUE": VALUE, "YOJ": YOJ, "DEROG": DEROG,
+        "DELINQ": DELINQ, "CLAGE": CLAGE, "NINQ": NINQ, "CLNO": CLNO, "DEBTINC":
+        DEBTINC}, index=index
+    )
+    input_array = impute_missing_values(input_array)
+    prediction = model.predict_proba(input_array).tolist()
+
+    # Check for numpy values and convert to a CAS readable representation
+    if isinstance(prediction, np.ndarray):
+        prediction = prediction.tolist()
+
+    if input_array.shape[0] == 1:
+        if prediction[0][1] > 0.5:
+            EM_CLASSIFICATION = "1"
+        else:
+            EM_CLASSIFICATION = "0"
+        return EM_CLASSIFICATION, prediction[0][1]
+    else:
+        df = pd.DataFrame(prediction)
+        proba = df[1]
+        classifications = np.where(df[1] > 0.5, '1', '0')
+        return pd.DataFrame({'EM_CLASSIFICATION': classifications, 'EM_EVENTPROBABILITY': proba})
+
+def impute_missing_values(data):
+    impute_values = \
+        {'VALUE': 101776.04874145007, 'DEBTINC': 33.779915349235246, 'NINQ':
+        1.1860550458715597, 'DELINQ': 0.4494423791821561, 'CLAGE': 179.7662751900465,
+        'MORTDUE': 73760.817199559, 'DEROG': 0.2545696877380046, 'YOJ':
+        8.922268135904499, 'CLNO': 21.29609620076682, 'LOAN': 18607.96979865772}
+    return data.replace('           .', np.nan).fillna(impute_values).apply(pd.to_numeric, errors='ignore')
