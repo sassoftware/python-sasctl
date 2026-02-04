ee42ba8feee9277bf0f0c1d09bae2a88efde180f
djm21 <david.moore@sas.com>
2026-02-04 13:07:04 -0800

    fix: update imputation method to avoid warnings

--- src/sasctl/pzmm/write_score_code.py
+++ src/sasctl/pzmm/write_score_code.py
@@ -766,10 +766,15 @@ def impute_missing_values(data):
         """
     impute_values = \\\n + {"var1": 0, "var2": "", "var3": 125.3}
         """
-        self.score_code += f"\n{'':4}return data.replace('           .', np.nan).fillna(impute_values).apply(pd.to_numeric, errors='ignore')\n"
+        self.score_code += f"\n\n{'':4}# Specify downcasting behavior for pandas 2.x to avoid warnings\n"
+        self.score_code += f"{'':4}if int(pd.__version__.split('.')[0]) == 2:\n{'':8}pd.set_option('future.no_silent_downcasting', True)\n"
+        self.score_code += f"{'':4}return data.replace(r'^\\s*\\.$', np.nan, regex=True).fillna(impute_values).infer_objects()\n"
         """
         
-    return data.replace('           .', np.nan).fillna(impute_values).apply(pd.to_numeric, errors='ignore')
+    # Specify downcasting behavior for pandas 2.x to avoid warnings
+    if int(pd.__version__.split('.')[0]) == 2:
+        pd.set_option('future.no_silent_downcasting', True)
+    return data.replace(r'^\s*\.$', np.nan, regex=True).fillna(impute_values).infer_objects()
         """
 
     # TODO: Needs unit test
